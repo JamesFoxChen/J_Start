b1a0f6f88999450c832baf8612afab20b28db0ca
JamesFoxChen <25717594@qq.com>
2015-05-06 11:12:35 +0800

    add MongoTestadd

--- src/Start.java
+++ src/Start.java
@@ -4,10 +4,17 @@ public class Start {
 	public static void main(String[] args) {
 		 //System.out.println("Hello, world");
 
+		String result="";
 		 
 		 
-		String url="http://localhost:10083/v2/bizvouchers/getlist?nosign=1";
-		HttpUtil.HttpPost(url);
+//		String url="http://localhost:10083/v2/bizvouchers/getlist?nosign=1";
+//		HttpUtil.HttpPost(url);
+//		String result=StringUtils.AppendCurrentDate("Hello");
+//		System.out.println(result);
+		
+//		result=RedisTest.Simple();
+//	    System.out.println(result);
+		MongoTest.Simple();
 	}
 
 }
