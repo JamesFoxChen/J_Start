260f0206da016a2ffda011b99524003f667a4b20
25717594@qq.com <25717594@qq.com>
2015-05-06 21:18:53 +0800

    add reflection class

--- src/Start.java
+++ src/Start.java
@@ -5,35 +5,37 @@ public class Start {
     public static void main(String[] args) {
         //System.out.println("Hello, world");
 
+        ReflectionUtil reflect=new ReflectionUtil();
+        reflect.Entrance();
         String result = "";
 
 
 //		String url="http://localhost:10083/v2/bizvouchers/getlist?nosign=1";
 //		HttpUtil.HttpPost(url);
-//		String result=StringUtils.AppendCurrentDate("Hello");
-//		System.out.println(result);
+//		 result=StringUtils.AppendCurrentDate("Hello");
+//     	System.out.println(result);
 
 //		result=RedisTest.Simple();
 //	    System.out.println(result);
 //		MongoTest.Simple();
 
-        try {
-            PropertiesUtil p = new PropertiesUtil();
-            result = p.readValue("username");
-            System.out.print(result);
-
-            System.out.println();
-            p.setValue("username123", "James123m");
-            result = p.readValue("username123");
-            System.out.print(result);
-
-            System.out.println();
-            p.updateValue("username123", "James333");
-            result = p.readValue("username123");
-            System.out.print(result);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            PropertiesUtil p = new PropertiesUtil();
+//            result = p.readValue("username");
+//            System.out.print(result);
+//
+//            System.out.println();
+//            p.setValue("username123", "James123m");
+//            result = p.readValue("username123");
+//            System.out.print(result);
+//
+//            System.out.println();
+//            p.updateValue("username123", "James333");
+//            result = p.readValue("username123");
+//            System.out.print(result);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
