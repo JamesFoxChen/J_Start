0d639b663840cd9c03e39e646ef2ae2a921544cc
JamesFoxChen <25717594@qq.com>
2015-05-08 14:40:12 +0800

    add

--- src/Start.java
+++ src/Start.java
@@ -2,12 +2,14 @@ import java.io.IOException;
 
 public class Start {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //System.out.println("Hello, world");
 
         String result = "";
 
-
+//        RabbitMQUtil.SendGet("http://localhost:15672/api/overview");
+        String msg="{\"properties\":{},\"routing_key\":\"logs\",\"payload\":\"my body\",\"payload_encoding\":\"string\"}";
+        RabbitMQUtil.SendPost("http://localhost:15672/api/exchanges/%2F/logs/publish",msg);
 //		String url="http://localhost:10083/v2/bizvouchers/getlist?nosign=1";
 //		HttpUtil.HttpPost(url);
 //		String result=StringUtils.AppendCurrentDate("Hello");
@@ -17,23 +19,23 @@ public class Start {
 //	    System.out.println(result);
 //		MongoTest.Simple();
 
-        try {
-            PropertiesUtil p = new PropertiesUtil();
-            result = p.readValue("username");
-            System.out.print(result);
-
-            System.out.println();
-            p.setValue("username123", "James123m");
-            result = p.readValue("username123");
-            System.out.print(result);
-
-            System.out.println();
-            p.updateValue("username123", "James333");
-            result = p.readValue("username123");
-            System.out.print(result);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            PropertiesUtil p = new PropertiesUtil();
+//            result = p.readValue("username");
+//            System.out.print(result);
+//
+//            System.out.println();
+//            p.setValue("username123", "James123m");
+//            result = p.readValue("username123");
+//            System.out.print(result);
+//
+//            System.out.println();
+//            p.updateValue("username123", "James333");
+//            result = p.readValue("username123");
+//            System.out.print(result);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
